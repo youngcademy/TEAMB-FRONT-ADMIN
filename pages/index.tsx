cdda74969a142eb486040a04fab5e3fea4052b1c
jh <97775986+jhp5234@users.noreply.github.com>
2022-02-09 00:42:30 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
     return (
-        <div className={styles.container}>
+        <>
             <h3>홈페이지</h3>
-        </div>
+        </>
     );
 };
 
