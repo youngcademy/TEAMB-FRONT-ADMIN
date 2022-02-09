2e77de53fa893022433619d8d5ed573ef9015b8a
jh <97775986+jhp5234@users.noreply.github.com>
2022-02-09 14:39:42 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css';
+import '@coreui/coreui/dist/css/coreui.min.css';
+
 import type { AppProps } from 'next/app';
 import Layout from '../layout/Layout';
-
-import '@coreui/coreui/dist/css/coreui.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
