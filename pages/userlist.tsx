867110c76465881d6066b9ff44bb29926ecbb404
gkssk1026 <gkssk1026@hanmail.net>
2022-02-21 19:50:28 +0900

    값수정

--- pages/userlist.tsx
+++ pages/userlist.tsx
@@ -2,47 +2,44 @@ import * as React from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'id', headerName: 'No', width: 90 },
     {
-        field: 'firstName',
-        headerName: 'First name',
+        field: 'ID',
+        headerName: 'ID',
         width: 150,
         editable: true,
     },
     {
-        field: 'lastName',
-        headerName: 'Last name',
+        field: 'Name',
+        headerName: 'Name',
         width: 150,
         editable: true,
     },
     {
-        field: 'age',
-        headerName: 'Age',
+        field: 'Total',
+        headerName: 'Total',
         type: 'number',
         width: 110,
         editable: true,
     },
     {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params: GridValueGetterParams) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+        field: 'Grade',
+        headerName: 'Grade',
+        width: 150,
+        editable: true,
     },
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, ID: 'Snow', Name: 'Jon', Total: 3500000, Grade: '우수' },
+    { id: 2, ID: 'Lannister', Name: 'Cersei', Total: 42000, Grade: '일반' },
+    { id: 3, ID: 'Lannister', Name: 'Jaime', Total: 45000, Grade: '일반' },
+    { id: 4, ID: 'Stark', Name: 'Arya', Total: 16000, Grade: '기업' },
+    { id: 5, ID: 'Targaryen', Name: 'Daenerys', Total: null, Grade: '신규' },
+    { id: 6, ID: 'Melisandre', Name: null, Total: 150000, Grade: '일반' },
+    { id: 7, ID: 'Clifford', Name: 'Ferrara', Total: 44000, Grade: '일반' },
+    { id: 8, ID: 'Frances', Name: 'Rossini', Total: 36000, Grade: '기업' },
+    { id: 9, ID: 'Roxie', firstName: 'Harvey', Total: 65000, Grade: '일반' },
 ];
 
 export default function DataGridDemo() {
