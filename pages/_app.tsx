78f0f44474e9838625d28bcf4651d0f6d138c1e1
jh <97775986+jhp5234@users.noreply.github.com>
2022-02-24 21:05:46 +0900

    리덕스 설치 및 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,16 @@ import '@coreui/coreui/dist/css/coreui.min.css';
 
 import type { AppProps } from 'next/app';
 import Layout from '../layout/Layout';
+import { store } from '../app/store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />{' '}
-        </Layout>
+        <Provider store={store}>
+            <Layout>
+                <Component {...pageProps} />{' '}
+            </Layout>
+        </Provider>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 
+import { RootState } from '../app/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../features/counter/counterSlice';
+
 const Home: NextPage = () => {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
     return (
         <>
             <h3>제목: 홈페이지</h3>
+            <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}
+            >
+                Increment
+            </button>
+            <span>{count}</span>
+            <button
+                aria-label="Decrement value"
+                onClick={() => dispatch(decrement())}
+            >
+                Decrement
+            </button>
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Repudiandae iste error, maiores minus magnam dignissimos ratione
