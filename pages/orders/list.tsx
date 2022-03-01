e10fb15073adb91bb2c5b19df4f1359590ca320a
goops2000 <goops2000@naver.com>
2022-03-01 21:20:06 +0900

    orderheader컴포넌트 list page에 넣기

--- pages/orders/list.tsx
+++ pages/orders/list.tsx
@@ -1,8 +1,86 @@
-import { CButton, CCard } from '@coreui/react';
+import React from 'react';
+import { useState } from 'react';
+import {
+    CCard,
+    CNavItem,
+    CContainer,
+    CButton,
+    CFormSelect,
+    CFormInput,
+    CInputGroup,
+    CDropdown,
+    CNavbarNav,
+    CCollapse,
+    CNavbarBrand,
+    CNavbar,
+    CNavbarToggler,
+} from '@coreui/react';
 import type { NextPage } from 'next';
-import OrderHeader from '../../components/OrderHeader';
 import DataTable, { TableColumn } from 'react-data-table-component';
 import Link from 'next/link';
+
+const OrderHeader = () => {
+    const OrderChange = () => {
+        return (
+            <CFormSelect aria-label="Default select example">
+                <option>상태</option>
+                <option value="1">입금</option>
+                <option value="2">결제완료</option>
+                <option value="3">상품준비중</option>
+                <option value="4">배송</option>
+                <option value="5">구매확정</option>
+                <option value="6">취소요청</option>
+                <option value="7">반품요청</option>
+                <option value="8">교환요청</option>
+                <option value="9">환불완료</option>
+                <option value="10">교환완료</option>
+            </CFormSelect>
+        );
+    };
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <>
+            <CNavbar expand="lg" colorScheme="dark" className="bg-dark">
+                <CContainer fluid>
+                    <CNavbarBrand href="#">주문리스트</CNavbarBrand>
+                    <CNavbarToggler
+                        aria-label="Toggle navigation"
+                        aria-expanded={visible}
+                        onClick={() => setVisible(!visible)}
+                    />
+                    <CCollapse className="navbar-collapse" visible={visible}>
+                        <CNavbarNav>
+                            <CNavItem>
+                                <CDropdown variant="nav-item" popper={false}>
+                                    <OrderChange />
+                                </CDropdown>
+                            </CNavItem>
+                            <CFormSelect aria-label="Default select example">
+                                <option>검색항목</option>
+                                <option value="1">주문검색</option>
+                                <option value="2">상품검색</option>
+                            </CFormSelect>
+                        </CNavbarNav>
+                        <CInputGroup>
+                            <CFormInput
+                                type="search"
+                                placeholder="검색"
+                                aria-label="주문검색 with two button addons"
+                            />
+                            <CButton type="submit" color="secondary">
+                                조회
+                            </CButton>
+                            <CButton type="submit" color="primary">
+                                초기화
+                            </CButton>
+                        </CInputGroup>
+                    </CCollapse>
+                </CContainer>
+            </CNavbar>
+        </>
+    );
+};
 
 const OrderList: NextPage = () => {
     const DetailButton = function () {
