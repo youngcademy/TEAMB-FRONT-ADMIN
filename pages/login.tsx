737532af61481593d5f0cd37948d58b34ad28c0e
jh <97775986+jhp5234@users.noreply.github.com>
2022-03-17 20:24:28 +0900

    axios 로그인 api 추가

---
+++ pages/login.tsx
@@ -0,0 +1,68 @@
+import type { NextPage } from 'next';
+import React, { useRef } from 'react';
+import {
+    CHeader,
+    CContainer,
+    CHeaderBrand,
+    CCard,
+    CCardHeader,
+    CCardBody,
+    CCardTitle,
+    CCardText,
+    CButton,
+    CForm,
+    CFormLabel,
+    CFormInput,
+    CFormText,
+    CRow,
+    CCol,
+    CFormSelect,
+    CTable,
+    CTableHead,
+    CTableRow,
+    CTableHeaderCell,
+    CTableBody,
+    CTableDataCell,
+} from '@coreui/react';
+
+import axios from 'axios';
+
+const Login: NextPage = () => {
+    const inputID = useRef(null);
+    const inputPW = useRef(null);
+    function handleClick() {
+        axios
+            .post('http://localhost:1234/login', {
+                userID: inputID.current.value,
+                userPW: inputPW.current.value,
+            })
+            .then(function (response) {
+                console.log(response.data.accessToken);
+            })
+            .catch(function (error) {
+                console.log(error);
+                console.log('에러에러');
+            });
+    }
+    return (
+        <>
+            <CForm className="row g-3">
+                <CCol md={6}>
+                    <CFormLabel>ID</CFormLabel>
+                    <CFormInput type="text" id="inputID" ref={inputID} />
+                </CCol>
+                <CCol md={6}>
+                    <CFormLabel>Password</CFormLabel>
+                    <CFormInput
+                        type="password"
+                        id="inputPassword"
+                        ref={inputPW}
+                    />
+                </CCol>
+            </CForm>
+            <CButton onClick={handleClick}>제출</CButton>
+        </>
+    );
+};
+
+export default Login;
